7c7d6b0f9990764a08ead1b1238ae6f56386ac4f
HossahinDev <hossahin321@gmail.com>
2025-06-05 17:16:58 +0600

    make post api and same problem fix

--- index.js
+++ index.js
@@ -21,8 +21,18 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const VolunteerDB = client.db("VolunteerDB");
+    const userData = VolunteerDB.collection("userData");
+
+    app.post("/signup", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await userData.insertOne(data);
+      res.send({ message: "Data Insert Success", data: result });
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -30,7 +40,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
